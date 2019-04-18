697e6f11c3b609b0a8d846ea7a16a54ef9cf26e1
氢键 <42627185+JoveH-H@users.noreply.github.com>
2019-04-18 18:39:55 +0800

    Update Kalman_filtering.py

--- Kalman_filtering.py
+++ Kalman_filtering.py
@@ -5,15 +5,17 @@ delta_t = 0.1                               # 每秒钟采一次样
 end_t = 8                                   # 时间长度
 time_t = end_t * 10                         # 采样次数
 t = np.arange(0, end_t, delta_t)            # 设置时间数组
+
 u = 10                                      # 定义外界对系统的作用 加速度
 x = 1 / 2 * u * t ** 2                      # 实际真实位置
 
+v_var = 7                                   # 测量噪声的方差
 # 创建高斯噪声，精确到小数点后两位
-v_var = 7                                  # 测量噪声的方差
-v = np.mat(np.round(np.random.normal(0, v_var, time_t), 2))
+v_noise = np.round(np.random.normal(0, v_var, time_t), 2)
 
-z = x + v                                   # 定义测量值（假设测量值=实际状态值+噪声）
 X = np.mat([[0], [0]])                      # 定义预测优化值的初始状态
+v = np.mat(v_noise)                         # 定义测量噪声
+z = x + v                                   # 定义测量值（假设测量值=实际状态值+噪声）
 A = np.mat([[1, delta_t], [0, 1]])          # 定义状态转移矩阵
 B = [[1 / 2 * (delta_t ** 2)], [delta_t]]   # 定义输入控制矩阵
 P = np.mat([[1, 0], [0, 1]])                # 定义初始状态协方差矩阵
