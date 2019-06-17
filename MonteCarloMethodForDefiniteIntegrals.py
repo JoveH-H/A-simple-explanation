aeb87916933c5228e852ef9f3891eabb984d2d77
氢键 <42627185+JoveH-H@users.noreply.github.com>
2019-06-17 16:36:31 +0800

    Update MonteCarloMethodForDefiniteIntegrals.py

--- MonteCarloMethodForDefiniteIntegrals.py
+++ MonteCarloMethodForDefiniteIntegrals.py
@@ -4,8 +4,8 @@ n = 300000  # 总投点数
 count = 0  # 位于曲线之下的投点数
 
 # 投点x,y的范围
-x_min, x_max = 0.0, 1.0
-y_min, y_max = 0.0, 1.0
+x_min, x_max = 1.0, 2.0
+y_min, y_max = 0.0, 8.0
 
 for i in range(0, n):
 
@@ -17,6 +17,6 @@ for i in range(0, n):
     if x * x * x >= y:
         count += 1
 
-# 所求的积分值即为曲线下方的面积与正方形面积1的比
-result = count / float(n)
+# 所求的积分值即为曲线下方的面积与正方形面积8的比
+result = count / float(n) * 8
 print("result is ", result)
