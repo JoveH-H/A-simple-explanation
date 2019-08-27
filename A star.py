74668cf2fb701c26b568d4d22662be5e6f418489
氢键 <42627185+JoveH-H@users.noreply.github.com>
2019-08-27 14:55:30 +0800

    Update A star.py

--- A star.py
+++ A star.py
@@ -22,7 +22,7 @@ class Array2D:
 
 class Point:
     """
-    表示一个点
+    表示一个结点
     """
 
     def __init__(self, x, y):
@@ -40,22 +40,23 @@ class Point:
 
 class AStar:
     """
-    AStar算法的Python3.x实现
+    AStar算法，启发式函数：曼哈顿距离
     """
 
     class Node:  # 描述AStar算法中的节点数据
-        def __init__(self, point, endPoint, g=0):
+        def __init__(self, point, goalPoint, g=0):
             self.point = point  # 自己的坐标
             self.father = None  # 父节点
-            self.g = g  # g值，g值在用到的时候会重新算
-            self.h = (abs(endPoint.x - point.x) + abs(endPoint.y - point.y)) * 10  # 计算h值
+            self.g = g  # g值，当前已产生的代价
+            self.D = 10  # 曼哈顿距离的D倍
+            self.h = (abs(point.x - goalPoint.x) + abs(point.y - goalPoint.y)) * self.D  # h值，未来可能产生的代价（曼哈顿距离）
 
-    def __init__(self, map2d, startPoint, endPoint, passTag=0):
+    def __init__(self, map2d, startPoint, goalPoint, passTag=0):
         """
         构造AStar算法的启动条件
         :param map2d: Array2D类型的寻路数组
         :param startPoint: Point或二元组类型的寻路起点
-        :param endPoint: Point或二元组类型的寻路终点
+        :param goalPoint: Point或二元组类型的寻路终点
         :param passTag: int类型的可行走标记（若地图数据!=passTag即为障碍）
         """
         # 开启表
@@ -65,20 +66,19 @@ class AStar:
         # 寻路地图
         self.map2d = map2d
         # 起点终点
-        if isinstance(startPoint, Point) and isinstance(endPoint, Point):
+        if isinstance(startPoint, Point) and isinstance(goalPoint, Point):
             self.startPoint = startPoint
-            self.endPoint = endPoint
+            self.goalPoint = goalPoint
         else:
             self.startPoint = Point(*startPoint)
-            self.endPoint = Point(*endPoint)
+            self.goalPoint = Point(*goalPoint)
 
         # 可行走标记
         self.passTag = passTag
 
     def getMinNode(self):
         """
-        获得openlist中F值最小的节点
-        :return: Node
+        获得openlist中F值最小的结点
         """
         currentNode = self.openList[0]
         for node in self.openList:
@@ -87,20 +87,29 @@ class AStar:
         return currentNode
 
     def pointInCloseList(self, point):
+        """
+        判断结点是否在CloseList
+        """
         for node in self.closeList:
             if node.point == point:
                 return True
         return False
 
     def pointInOpenList(self, point):
+        """
+        判断结点是否在OpenList
+        """
         for node in self.openList:
             if node.point == point:
                 return node
         return None
 
-    def endPointInCloseList(self):
+    def goalPointeInCloseList(self):
+        """
+        判断目标结点是否在CloseList
+        """
         for node in self.openList:
-            if node.point == self.endPoint:
+            if node.point == self.goalPoint:
                 return node
         return None
 
@@ -113,7 +122,8 @@ class AStar:
         :return:
         """
         # 越界检测
-        if minF.point.x + offsetX < 0 or minF.point.x + offsetX > self.map2d.w - 1 or minF.point.y + offsetY < 0 or minF.point.y + offsetY > self.map2d.h - 1:
+        if minF.point.x + offsetX < 0 or minF.point.x + offsetX > self.map2d.w - 1 or \
+                minF.point.y + offsetY < 0 or minF.point.y + offsetY > self.map2d.h - 1:
             return
         # 如果是障碍，就忽略
         if self.map2d[minF.point.x + offsetX][minF.point.y + offsetY] != self.passTag:
@@ -130,7 +140,7 @@ class AStar:
         # 如果不再openList中，就把它加入openlist
         currentNode = self.pointInOpenList(currentPoint)
         if not currentNode:
-            currentNode = AStar.Node(currentPoint, self.endPoint, g=minF.g + step)
+            currentNode = AStar.Node(currentPoint, self.goalPoint, g=minF.g + step)
             currentNode.father = minF
             self.openList.append(currentNode)
             return
@@ -145,11 +155,11 @@ class AStar:
         :return: None或Point列表（路径）
         """
         # 判断寻路终点是否是障碍
-        if self.map2d[self.endPoint.x][self.endPoint.y] != self.passTag:
+        if self.map2d[self.goalPoint.x][self.goalPoint.y] != self.passTag:
             return None
 
         # 1.将起点放入开启列表
-        startNode = AStar.Node(self.startPoint, self.endPoint)
+        startNode = AStar.Node(self.startPoint, self.goalPoint)
         self.openList.append(startNode)
         # 2.主循环逻辑
         while True:
@@ -164,7 +174,7 @@ class AStar:
             self.searchNear(minF, -1, 0)
             self.searchNear(minF, 1, 0)
             # 判断是否终止
-            point = self.endPointInCloseList()
+            point = self.goalPointeInCloseList()
             if point:  # 如果终点在关闭表中，就返回结果
                 # print("关闭表中")
                 cPoint = point
@@ -194,18 +204,18 @@ def Display_map(map, start=None, goal=None, title=None):
     plt.xticks(np.arange(0, map.w, 1))
     plt.yticks(np.arange(0, map.h, 1))
     plt.grid(lw=2)
-    obstacles_listx, obstacles_listy = [], []
+    obstacles_x, obstacles_y = [], []
     pathx, pathy = [], []
     for x in range(map.w):
         for y in range(map.h):
             if map[x][y] == 1:
-                obstacles_listx.append(x)
-                obstacles_listy.append(y)
+                obstacles_x.append(x)
+                obstacles_y.append(y)
             elif map[x][y] == 'o':
                 pathx.append(x)
                 pathy.append(y)
-    if obstacles_listx != []:
-        plt.plot(obstacles_listx, obstacles_listy, 'xr', markersize=10, label='障碍')
+    if obstacles_x != []:
+        plt.plot(obstacles_x, obstacles_y, 'xr', markersize=10, label='障碍')
     if pathx != []:
         plt.plot(pathx, pathy, 'og', markersize=10, label='路径')
     if start != None:
@@ -222,12 +232,14 @@ if __name__ == '__main__':
     # 创建一个10*10的地图
     mapw, maph = 10, 10
     map2d = Array2D(mapw, maph)
+
     # 设置障碍
     obstacle = [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6]]
     for i in obstacle:
         map2d[i[0]][i[1]] = 1
     # 显示地图设置障碍后的样子
     Display_map(map2d, title="设置障碍")
+
     # 设置起点为，终点
     startx, starty = 0, 0
     goalx, goaly = 9, 4
@@ -235,6 +247,7 @@ if __name__ == '__main__':
     aStar = AStar(map2d, Point(startx, starty), Point(goalx, goaly))
     # 开始寻路
     pathList = aStar.start()
+
     # 遍历路径点,在map2d上以'o'表示
     for point in pathList:
         map2d[point.x][point.y] = 'o'
